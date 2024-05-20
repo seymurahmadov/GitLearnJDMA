b1ba9e887c387fcaa8fe4a017cbfc89de132921e
seymurahmadov <s.axmedov99@gmail.com>
2024-05-20 13:50:17 +0400

    Aslan taski hazirdi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Aslanin kodu");
+        for (int i = 0; i < 50; i++) {
+            System.out.println(i);
+        }
     }
 }
